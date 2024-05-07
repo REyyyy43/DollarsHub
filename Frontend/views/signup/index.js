99bb8820375f0cd4b622c8587c146921038574eb
Reinaldo <landaetareinaldo143@gmail.com>
2024-05-07 12:07:22 -0400

    tree commit

--- Frontend/views/signup/index.js
+++ Frontend/views/signup/index.js
@@ -1,29 +1,24 @@
-import { createNotification } from '../Components/notification.js';
-
-const form = document.querySelector('#form');
-const nameInput = document.querySelector('#name-input');
-const emailInput = document.querySelector('#email-input');
-const passwordInput = document.querySelector('#password-input');
-const matchInput = document.querySelector('#match-input');
-const formBtn = document.querySelector('#form-btn');
-const notification = document.querySelector('#notification');
-
 //Rgex validation
-
 const EMAIL_VALIDATION = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
 const PASSWORD_VALIDATION = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 const NAME_VALIDATION = /^[A-Z\u00d1][a-zA-Z-ÿ\u00f1\u00d1]+(\s*[A-Z\u00d1][a-zA-Z-ÿ\u00f1\u00d1\s]*)$/;
-const NUMBER_VALIDATION = /^[0-9]{6,16}$/;
+const PHONE_VALIDATION = /^[0-9]{6,16}$/;
+
+//SElectors
+const countries = document.querySelector("#countries");
+[...countries].forEach(option => {
+    option.innerHTML = option.innerHTML.split('(')[0];
+});
 
 // Validacion
 let nameValidation = false;
 let emailValidation = false;
 let passwordValidation = false;
 let matchValidation = false;
-let numberValidation = false;
+let phoneValidation = false;
 
 const validation = (input , regexValidation) => {
-    formBtn.disabled = !nameValidation || !emailValidation || !passwordValidation || !matchValidation;
+    formBtn.disabled = !nameValidation || !emailValidation || !passwordValidation || !matchValidation || !phoneValidation;
 
     if (input.value === '') {
         input.classList.remove('outline-red-500', 'outline-2' , 'outline' );
@@ -50,6 +45,11 @@ emailInput.addEventListener('input', e => {
     validation(emailInput, emailValidation);
 });
 
+phoneInput.addEventListener('input', e => {
+    phoneValidation = PHONE_VALIDATION.test(e.target.value);
+    validation(phoneInput, phoneValidation);
+});
+
 passwordInput.addEventListener('input', e => {
     passwordValidation = PASSWORD_VALIDATION.test(e.target.value);
     matchValidation = e.target.value === matchInput.value;
@@ -60,41 +60,5 @@ passwordInput.addEventListener('input', e => {
 matchInput.addEventListener('input', e => {
     matchValidation = e.target.value === passwordInput.value;
     validation(matchInput, matchValidation);
-});
-
-form.addEventListener('submit', async e => {
-e.preventDefault();
-try {
-    const newUser = {
-        name: nameInput.value,
-        email: emailInput.value,
-        password: passwordInput.value,
-    };
-    const { data } = await axios.post('/api/users' , newUser);
- createNotification(false, data);
-setTimeout(() => {
-    notification.innerHTML = '';
-}, 5000);
-
-nameInput.value = '';
-emailInput.value = '';
-passwordInput.value = '';
-matchInput.value = '';
-validation(nameInput, false);
-validation(emailInput, false);
-validation(passwordInput, false);
-validation(matchInput, false);
-
-
-} catch (error) {
-    createNotification(true, error.response.data.error);
-    setTimeout(() => {
-        notification.innerHTML = '';
-    }, 5000);
-   
-  //cargar el error 
-    // console.log(error.response.data.error);
-}
-
 });
 
