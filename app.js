1e81ef56cd885ef6c24697713da4e7c137d4bb28
Reinaldo <landaetareinaldo143@gmail.com>
2024-05-19 13:14:20 -0400

    tree commit

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ const loginRouter = require('./controllers/login');
 const coinRouter = require('./controllers/coin');
 const coinPageRouter = require('./controllers/coinPage');
 const { userExtractor } = require('./middleware/auth');
-
+const { MONGO_URI } = require('./config');
 
 (async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI_TEST);
+        await mongoose.connect(MONGO_URI);
         console.log('Conectado a MongoDB');
     } catch (error) {
         console.log(error);
--- config.js
+++ config.js
@@ -3,3 +3,9 @@ const PAGE_URL = process.env.NODE_ENV === 'production'
 : 'http://localhost:3004'
 
 module.exports = { PAGE_URL };
+
+const MONGO_URI = process.env.NODE_ENV === 'production'
+? process.env.MONGO_URI_PROD
+: process.env.MONGO_URI_TEST
+
+module.exports = { PAGE_URL, MONGO_URI };
