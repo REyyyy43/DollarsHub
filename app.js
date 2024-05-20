07f2cc90219542e65b61e47a6e474c96d3849bc3
Reinaldo <landaetareinaldo143@gmail.com>
2024-05-20 03:07:23 -0400

    tree commit

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(morgan('tiny')); // Mantenemos el logger de Morgan
 // Rutas frontend
 app.use('/', express.static(path.resolve('views', 'Home')));
 app.use('/signup', express.static(path.resolve('views', 'signup')));
+app.use('/styles', express.static(path.resolve('views', 'styles')));
 app.use('/login', express.static(path.resolve('views', 'login')));
 app.use('/Page', express.static(path.resolve('views', 'Page')));
 app.use('/verify/:id/:token', express.static(path.resolve('views', 'verify')));
